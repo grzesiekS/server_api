01d02990b3df80799028d1505fc688b0105da230
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-08 20:50:37 +0200

    Add put endpoint for /testimonials/:id

--- server.js
+++ server.js
@@ -23,6 +23,25 @@ app.get('/testimonials/:id', (req, res) => {
     res.send(db.filter(content => content.id === parseInt(req.params.id)));
 });
 
+app.post('/testimonials', (req, res) => {
+    const newId = db[db.length-1].id + 1;
+    db.push({
+       id: newId,
+       author: req.body.author,
+       text: req.body.text, 
+    });
+    res.send({message: 'OK'});
+});
+
+app.put('/testimonials/:id', (req, res) => {
+    db.filter(content => content.id === parseInt(req.params.id)).forEach(content => {
+        content.author = req.body.author;
+        content.text = req.body.text;
+    });
+    
+    res.send({message: 'OK'});
+});
+
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
 });
