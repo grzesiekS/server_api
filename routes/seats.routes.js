7e1c1e13e206bee7df0a144da06320fa68909582
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-08 22:03:44 +0200

    Add seats.routes.js

---
+++ routes/seats.routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+
+const filterDB = id => (
+    db.seats.filter(content => content.id == id)
+);
+
+router.route('/seats').get((req, res) => {
+    res.json(db.seats);
+});
+
+router.route('/seats/random').get((req, res) => {
+    const randomNumber = Math.floor(Math.random() * db.seats.length) + 1
+    res.json(filterDB(randomNumber));
+});
+
+router.route('/seats/:id').get((req, res) => {
+    res.json(filterDB(req.params.id));
+});
+
+router.route('/seats').post((req, res) => {
+    const newId = db.seats[db.seats.length-1].id + 1;
+    db.seats.push({
+       id: newId,
+       day: req.body.day,
+       seat: req.body.seat,
+       client: req.body.client,
+       email: req.body.email,
+    });
+    res.json({message: 'OK'});
+});
+
+router.route('/seats/:id').put((req, res) => {
+    filterDB(req.params.id).forEach(content => {
+        content.day = req.body.day;
+        content.seat = req.body.seat;
+        content.client = req.body.client;
+        content.email = req.body.email;
+    });
+    
+    res.json({message: 'OK'});
+});
+
+router.route('/seats/:id').delete((req, res) => {
+    if(db.seats.indexOf(filterDB(req.params.id)[0]) > - 1) db.seats.splice(db.seats.indexOf(filterDB(req.params.id)[0]), 1);
+    res.json({message: 'OK'});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/', testimonialsRoutes);
 app.use('/', concertsRoutes);
+app.use('/', seatsRoutes);
 
 app.use((req, res) => {
     res.status(404).json({message: 'Not found...'})
